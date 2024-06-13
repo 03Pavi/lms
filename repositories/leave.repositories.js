aee4435e1e7aab989151d31e637c3da370746fc6
anmol sethi <anmol.1157@zenmonk.tech>
2024-06-13 19:25:00 +0530

    add get leave by orgId route

--- repositories/leave.repositories.js
+++ repositories/leave.repositories.js
@@ -1,23 +1,32 @@
-const { leave: leave_model } = require('../models');
+const { leave } = require('../models');
 
 const { sequelize } = require("../config/db_connection");
 const { base_repository } = require('./base.repositories');
 
 class leave_repository extends base_repository {
+
     constructor(payload) {
         super(payload);
     }
 
-    async create_leave ({leave, transaction}) {
-        let criteria = { organisation_id: leave.organisation_id }
-        console.log(leave);
-        return this.find_create_find(criteria, leave, transaction)
+    async get_leaves_by_organisation_id({organisation_id,limit,transaction}) {
+        let criteria = {
+            organisation_id: organisation_id
+        }
+        let include = [];
+        let attributes = {};
+        let order = [
+            ['id', 'DESC']
+        ];
+        return this.find_all({criteria, include, attributes, order, limit, transaction});
+
     }
+
 }
 
 module.exports = {
     leave_repository_obj: new leave_repository({
         db_connection: sequelize,
-        model: leave_model,
+        model: leave,
     }),
 };
