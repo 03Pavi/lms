097e6bba2b110ba025f281e05bbde218bec706cd
anmol.1157@zenmonk.tech <anmol.1157@zenmonk.tech>
2024-06-14 10:58:07 +0530

    add get leaves list api

--- routes/organisation.routes.js
+++ routes/organisation.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const {leave_policy_controllers} = require("../controllers");
+const {leave_policy_controllers, leave_controllers} = require("../controllers");
 
 router.post('/:uuid/leave-policies',leave_policy_controllers.create_leave_policy);
+router.get('/:uuid/leaves',leave_controllers.get_leaves_by_orgainisation_id);
 
 module.exports = router;
--- services/index.js
+++ services/index.js
@@ -3,5 +3,6 @@ module.exports = {
     session_service: require('./session.services'),
     academic_group_service: require('./academic_group.services'),
     leave_policy_service: require('./leave_policy.services'),
+    leave_service: require('./leave.service'),
     cloud_storage_service: require('./cloud_storage.services')
 }
